4d58f8c58b9bf141c60656956370af49f4046a8d
Sergey Postnikov <ser.post2017@gmail.com>
2023-08-16 20:01:10 +0300

    templates and views attached to 'bouquets' app

---
+++ flower_workshop/bouquets/urls.py
@@ -0,0 +1,16 @@
+from .views import card
+from .views import catalog
+from .views import quiz
+from .views import quiz_step
+from .views import result
+
+from django.urls import path
+
+urlpatterns = [
+    path('catalog/', catalog, name='catalog'),
+    path('result/', result, name='result'),
+    path('quiz/', quiz, name='quiz'),
+    path('quiz-step/', quiz_step, name='quiz-step'),
+    path('card/<int:pk>/', card, name='card'),
+
+]
--- flower_workshop/bouquets/views.py
+++ flower_workshop/bouquets/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def catalog(request):
+    return render(request, 'bouquets/catalog.html', context={}) 
+
+
+def card(request, pk): 
+    return render(request, 'bouquets/card.html', context={})
+
+
+def result(request): 
+    return render(request, 'bouquets/result.html', context={})
+
+
+def quiz(request):
+    return render(request, 'bouquets/quiz.html')
+
+
+def quiz_step(request):
+    return render(request, 'bouquets/quiz-step.html')
