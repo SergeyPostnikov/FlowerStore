95ab601dc408271656fd27d1ab0c2a21b03a21d1
val-os <valery@osotov.ru>
2023-08-16 23:36:17 +0700

    Add Event and Flower models

--- flower_workshop/bouquets/admin.py
+++ flower_workshop/bouquets/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Flower, Event
+
+
+@admin.register(Flower)
+class FlowerAdmin(admin.ModelAdmin):
+    search_fields = [
+        'name',
+    ]
+    list_display = [
+        'name',
+    ]
+    search_fields = [
+        'name',
+    ]
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    search_fields = [
+        'name',
+    ]
+    list_display = [
+        'name',
+    ]
+    search_fields = [
+        'name',
+    ]
