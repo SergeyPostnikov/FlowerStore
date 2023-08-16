051e8cc208bd423d96693f4cdd708ce64157c543
val-os <valery@osotov.ru>
2023-08-16 22:52:46 +0700

    Create User model

--- flower_workshop/users2/models.py
+++ flower_workshop/users2/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User2(AbstractUser):
+    on_vacation = models.BooleanField('в отпуске', default=False, db_index=True)
+
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
+
+    def __str__(self):
+        return '{0} - {1}. Email: {2}'.format(self.pk, self.first_name, self.email)
