5c4cf532eef56d990132da0e0cf2183fe43a9234
val-os <valery@osotov.ru>
2023-08-16 23:36:17 +0700

    Add Event and Flower models

--- flower_workshop/bouquets/models.py
+++ flower_workshop/bouquets/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    name = models.CharField(
+        'Название события',
+        max_length=100,
+        blank=True,
+        db_index=True,
+    )
+
+    class Meta:
+        verbose_name = 'Событие'
+        verbose_name_plural = 'События'
+
+    def __str__(self):
+        return self.name
+
+
+class Flower(models.Model):
+    name = models.CharField(
+        'Цветок',
+        max_length=100,
+        blank=True,
+        db_index=True,
+    )
+
+    class Meta:
+        verbose_name = 'Цветок'
+        verbose_name_plural = 'Виды цветов'
+
+    def __str__(self):
+        return self.name
