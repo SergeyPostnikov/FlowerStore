6a3ec8153ea58ef5b39b2d6f7167d745489a1586
Pavel2232 <106510078+Pavel2232@users.noreply.github.com>
2023-08-17 01:33:52 +0300

    add Model Order

--- flower_workshop/flower_workshop/settings.py
+++ flower_workshop/flower_workshop/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+
     # local
     'users2.apps.Users2Config',
     'bouquets.apps.BouquetsConfig',
--- flower_workshop/orders/views.py
+++ flower_workshop/orders/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
+from flower_workshop.orders.models import Order
+
 
 def order(request):
+
     return render(request, 'orders/order.html', context={})
 
 
