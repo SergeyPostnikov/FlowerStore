483b49c4f9a6dec254e68c1d84c5e24b8697cc67
Sergey Postnikov <ser.post2017@gmail.com>
2023-08-16 20:01:10 +0300

    templates and views attached to 'bouquets' app

--- flower_workshop/flower_workshop/urls.py
+++ flower_workshop/flower_workshop/urls.py
@@ -19,22 +19,13 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-from flower_store.views import card
-from flower_store.views import catalog
 from flower_store.views import consultation
-from flower_store.views import quiz
-from flower_store.views import quiz_step
-from flower_store.views import result
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', include('home.urls'), name='home'),
     path('order/', include('orders.urls'), name='order'),
-    path('catalog/', catalog, name='catalog'),
-    path('result/', result, name='result'),
-    path('quiz/', quiz, name='quiz'),
-    path('quiz-step/', quiz_step, name='quiz-step'),
-    path('card/<int:pk>/', card, name='card'),
+    path('bouquets/', include('bouquets.urls')),
     path('consultation/', consultation, name='consultation')
 ]
 
