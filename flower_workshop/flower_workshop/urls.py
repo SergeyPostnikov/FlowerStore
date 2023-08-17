998eb64a891881bc7dcf9e50e8b80c419a0e600a
val-os <valery@osotov.ru>
2023-08-17 14:36:36 +0700

    Add MEDIA urls

--- flower_workshop/flower_workshop/urls.py
+++ flower_workshop/flower_workshop/urls.py
@@ -19,7 +19,6 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', include('home.urls'), name='home'),
@@ -30,3 +29,4 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
