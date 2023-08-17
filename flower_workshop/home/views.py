268ad32a4853d6a79b2accf38e103b4145d59fe7
val-os <valery@osotov.ru>
2023-08-17 16:08:19 +0700

    Show recommended bouquets on homepage

--- flower_workshop/home/views.py
+++ flower_workshop/home/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
+from bouquets.models import Bouquet
+
 
 def index(request):
-    return render(request, 'home/index.html', context={})
+    return render(
+        request,
+        'home/index.html',
+        context={
+            'recommended_bouquets': Bouquet.objects.filter(recommended=True),
+        },
+    )
