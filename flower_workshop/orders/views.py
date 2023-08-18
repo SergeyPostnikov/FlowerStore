06e26feaf245c1cda68a81996ff97b3edd4f631c
Pavel2232 <106510078+Pavel2232@users.noreply.github.com>
2023-08-18 17:19:13 +0300

    add demo view

--- flower_workshop/orders/views.py
+++ flower_workshop/orders/views.py
@@ -8,26 +8,23 @@ from users2.models import User2
 
 
 def order_view(request):
-    # if request.method == 'GET':
-    #     user = User2.objects.last()
-    #     time = re.findall(r'(\d+:\d+)', request.GET.get('orderTime'))
-    #     # # < QueryDict: {'fname': ['dfsds'], 'tel': ['sdffsd'], 'adres': ['sdfsdffd'], 'orderTime': ['с 12:00 до 14:00']} >
-    #     order = Order.objects.create(
-    #         client=user,
-    #         bouquet= ...    ,
-    #         price=...  ,
-    #         from_delivery_time=time[0],
-    #         to_delivery_time=time[1],
-    #         address=request.GET.get('adres'),
-    #         phone=request.GET.get('tel'),
-    #         courier=user,
-    #         florist=user,
-    #
-    #     )
-    #     ok = 'с 12:00 до 14:00 '
-    #     o = re.findall(r'(\d+:\d+)', ok)
-    #     print(o[0])
-        # print(request.GET.get('fname'))
+    if request.method == 'GET':
+        user = User2.objects.last()
+        time = re.findall(r'(\d+:\d+)', request.GET.get('orderTime'))
+        # < QueryDict: {'fname': ['dfsds'], 'tel': ['sdffsd'], 'adres': ['sdfsdffd'], 'orderTime': ['с 12:00 до 14:00']} >
+        ##TODO подставить букет и цену
+        order = Order.objects.create(
+            client=user,
+            bouquet=...,
+            price=...,
+            from_delivery_time=time[0],
+            to_delivery_time=time[1],
+            address=request.GET.get('adres'),
+            phone=request.GET.get('tel'),
+            courier=user,
+            florist=user,
+
+        )
 
     return render(request, 'orders/order.html', context={})
 
