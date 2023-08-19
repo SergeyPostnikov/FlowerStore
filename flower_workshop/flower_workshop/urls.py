19990efffcf9e83e6c60aabbe44aa118e26979a4
val-os <valery@osotov.ru>
2023-08-19 16:31:00 +0700

    Add urls for error templates

--- flower_workshop/flower_workshop/urls.py
+++ flower_workshop/flower_workshop/urls.py
@@ -18,6 +18,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views import defaults as default_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,5 +29,10 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    urlpatterns += (
+        path("403/", default_views.permission_denied, kwargs={"exception": Exception("У вас нет доступа.")}),
+        path("404/", default_views.page_not_found, kwargs={"exception": Exception("Страница не найдена.")}),
+        path("500/", default_views.server_error),
+    )
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
