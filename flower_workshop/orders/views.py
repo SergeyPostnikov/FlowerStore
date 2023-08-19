13f95fa97ae73ba468a923a2e9e2a9fa24ec4f3a
val-os <valery@osotov.ru>
2023-08-19 19:55:41 +0700

    Make order button on card page works

--- flower_workshop/orders/views.py
+++ flower_workshop/orders/views.py
@@ -1,32 +1,41 @@
 import re
 
+from django.http import Http404
+from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 
+from bouquets.models import Bouquet
 from orders.models import Order
-
 from users2.models import User2
 
 
 def order_view(request):
     if request.method == 'GET':
         user = User2.objects.last()
-        time = re.findall(r'(\d+:\d+)', request.GET.get('orderTime'))
-        # < QueryDict: {'fname': ['dfsds'], 'tel': ['sdffsd'], 'adres': ['sdfsdffd'], 'orderTime': ['с 12:00 до 14:00']} >
-        ##TODO подставить букет и цену
-        order = Order.objects.create(
-            client=user,
-            bouquet=...,
-            price=...,
-            from_delivery_time=time[0],
-            to_delivery_time=time[1],
-            address=request.GET.get('adres'),
-            phone=request.GET.get('tel'),
-            courier=user,
-            florist=user,
-
-        )
-
-    return render(request, 'orders/order.html', context={})
+        bouquet_id = request.GET.get('bouquet_id')
+        print(bouquet_id)
+        if bouquet_id:
+            bouquet = get_object_or_404(Bouquet, pk=bouquet_id)
+            # time = re.findall(r'(\d+:\d+)', request.GET.get('orderTime'))
+            # < QueryDict: {'fname': ['dfsds'], 'tel': ['sdffsd'], 'adres': ['sdfsdffd'], 'orderTime': ['с 12:00 до 14:00']} >
+            ##TODO подставить букет и цену
+            # order = Order.objects.create(
+            #     client=user,
+            #     bouquet=...,
+            #     price=...,
+            #     # from_delivery_time=time[0],
+            #     # to_delivery_time=time[1],
+            #     address=request.GET.get('adres'),
+            #     phone=request.GET.get('tel'),
+            #     courier=user,
+            #     florist=user,
+            #
+            # )
+            context = {
+                'bouquet': bouquet,
+            }
+            return render(request, 'orders/order.html', context)
+        raise Http404
 
 
 def order_step(request):
